79a94d467e14c2b350e6d8b4f056df96aa81c8be
Eric Park <ericparkmusic@gmail.com>
2021-10-04 13:35:33 -0700

    Initial commit

---
+++ src/main/java/com/example/sneakyserver/SneakyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sneakyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sneakyserver/SneakyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sneakyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
